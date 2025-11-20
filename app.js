40f4f94abac6d1a45d17afb818e3e4049c6893c2
majdw1 <majdw12345@gmail.com>
2025-11-20 17:42:33 +0200

    commit inital

---
+++ app.js
@@ -0,0 +1,4 @@
+
+const logo = document.querySelector('.logo');
+logo.addEventListener('mousemove', ()=> logo.style.transform='scale(1.1)');
+logo.addEventListener('mouseleave', ()=> logo.style.transform='scale(1)');
